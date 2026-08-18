e2b9b9dd80427b54f19679327cf513037c2c70b9
Poojitha devabhakthuni <poojithadevabhakthuni@gmail.com>
2026-08-18 18:07:32 +0530

    Fix project structure and backend dependencies

--- Frontend/frontend/src/services/dashboardService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getDashboard = async () => {
-  const response = await api.get("dashboard/");
-  return response.data;
-};
--- Frontend/frontend/src/services/donationService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getDashboardData = async () => {
-  const response = await api.get("dashboard/");
-  return response.data;
-};
